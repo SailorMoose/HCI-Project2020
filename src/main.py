16c8a8d329d99d4b091bccdfb267d97ce080368e
Rasmus Olstedt <rasmus@docilitas.se>
2020-12-12 19:42:05 +0100

    You should Now be Able to launch the player standalone

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 # This Python file uses the following encoding: utf-8
-from PySide2.QtWidgets import QApplication
-from PySide2.QtQuick import QQuickView
-from PySide2.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQuick import QQuickView
+from PyQt5.QtCore import QUrl
 
 
 if __name__ == "__main__":
     app = QApplication([])
     view = QQuickView()
-    url = QUrl("VideoPlayer.qml.qml")
+    url = QUrl("VideoPlayer.qml")
 
     view.setSource(url)
     view.show()
